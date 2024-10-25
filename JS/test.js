2d7f974049b955539464571a3d83da9b7589bc51
Thibaut Genet <94045978+thibautgnt@users.noreply.github.com>
2024-10-25 14:08:28 +0200

    Update

---
+++ JS/test.js
@@ -0,0 +1,18 @@
+let student = {
+    name: "Marie",
+    age: 20,
+    courses: []
+};
+
+student.age = 21;
+student.grade = "A";
+
+student.courses.push("Math", "Physics", "Chemistry");
+
+let physicsIndex = student.courses.indexOf("Physics");
+
+let slicedCourses = student.courses.slice(0, 2);
+
+console.log(student);
+console.log(physicsIndex);
+console.log(slicedCourses);
